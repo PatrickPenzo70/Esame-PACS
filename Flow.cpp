ba4a6b1951e86b7a4e744076282bdf4086d9fe65
Patrick <patrick.penzo@polimi.it>
2024-11-06 23:19:49 +0100

    removed file Main-Balls-Config-Simulation-Flow

--- Flow.cpp
+++
@@ -1,9 +0,0 @@
-#include "Flow.h"
-
-Flow::Flow(double maxVelocity, double pipeRadius)
-    : maxVelocity(maxVelocity), pipeRadius(pipeRadius) {}
-
-double Flow::getVelocity(double y) const {
-    // Parabolic velocity profile (Poiseuille flow)
-    return maxVelocity * (1 - (y * y) / (pipeRadius * pipeRadius));
-}
